257ad0597629da9a5be6c00e12eeefe799e44ec3
Milena Ramiro <milena.ramiro@fmd.ag>
2025-02-03 21:01:15 -0300

    chore(Consulta): adiciona os setters

--- src/HealthMed.Domain/Entities/Consulta.cs
+++ src/HealthMed.Domain/Entities/Consulta.cs
@@ -2,6 +2,30 @@ namespace HealthMed.Domain.Entities;
 
 public class Consulta : Entidade
 {
+    public Consulta(Guid pacienteId, Guid medicoId, DateTime data, TimeSpan horario)
+    {
+        PacienteId = pacienteId;
+        MedicoId = medicoId;
+        Data = data;
+        Horario = horario;
+        Status = "Pendente";
+    }
+    
+    public void ConfirmarConsulta()
+    {
+        Status = "Confirmado";
+    }
+    
+    public void CancelarConsulta()
+    {
+        Status = "Cancelado";
+    }
+    
+    public void RecusarConsulta()
+    {
+        Status = "Recusado";
+    }
+
     public Guid PacienteId { get; set; }
     public Guid MedicoId { get; set; }
     public DateTime Data { get; set; }
