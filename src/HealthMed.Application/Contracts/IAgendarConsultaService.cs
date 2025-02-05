eeba054591545a8954d1029b872faefcdcccfca2
Milena Ramiro <milena.ramiro@fmd.ag>
2025-02-04 21:04:32 -0300

    chore(AgendarConsultaService): coda o agendamento

--- src/HealthMed.Application/Contracts/IAgendarConsultaService.cs
+++ src/HealthMed.Application/Contracts/IAgendarConsultaService.cs
@@ -1,6 +1,8 @@
+using HealthMed.Application.Commands;
+
 namespace HealthMed.Application.Contracts;
 
 public interface IAgendarConsultaService
 {
-    
+    Task Handle(AgendarConsultaCommand command);
 }
--- src/HealthMed.Application/Services/AgendarConsultaService.cs
+++ src/HealthMed.Application/Services/AgendarConsultaService.cs
@@ -1,8 +1,21 @@
+using HealthMed.Application.Commands;
 using HealthMed.Application.Contracts;
+using HealthMed.Domain.Entities;
 
 namespace HealthMed.Application.Services;
 
 public class AgendarConsultaService : IAgendarConsultaService
 {
-    
+    private IConsultaRepository _consultaRepository;
+
+    public AgendarConsultaService(IConsultaRepository consultaRepository)
+    {
+        _consultaRepository = consultaRepository;
+    }
+
+    public async Task Handle(AgendarConsultaCommand command)
+    {
+        var consulta = new Consulta(command.MedicoId, command.PacienteId, command.Data, command.Horario);
+        await _consultaRepository.Create(consulta);
+    }
 }
